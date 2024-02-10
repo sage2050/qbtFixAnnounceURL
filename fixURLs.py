9fa80cdb44b28f4d9d152d796d638f71a68e1dbe
sage2050 <48387376+sage2050@users.noreply.github.com>
2024-02-09 22:09:43 -0500

    Create fixURLs.py

---
+++ fixURLs.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+import qbittorrentapi
+
+badURL="[BROKEN ANNOUNCE URL HERE]"
+goodURL="[GOOD ANNOUNCE URL HERE]"
+
+conn_info = dict(
+    host="127.0.0.1",
+    port=8080,
+    username="admin",
+    password="adminadmin",
+)
+qbt_client = qbittorrentapi.Client(**conn_info)
+qbt_client.auth_log_in()
+
+for torrent in qbt_client.torrents_info():
+    for tracker in torrent.trackers:
+        if badURL in tracker.url:
+            torrent.add_tags(tags="trackerless")
+            torrent.remove_trackers(urls=[tracker.url])
+            print(f"Removed {tracker.url} from #{datetime.fromtimestamp(torrent.added_on)}# {torrent.name}")
+            
+
+for torrent in qbt_client.torrents_info():
+    if  "trackerless" in torrent.tags:
+        torrent.add_trackers(urls=goodURL)
+        torrent.remove_tags(tags="trackerless")
+        print(f"fixed {torrent.name}")
